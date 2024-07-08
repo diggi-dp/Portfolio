ea6b052a2b1654baa864ec4e12749f9205dcb680
diggi-dp <digpalsinghmandloi10@gmail.com>
2024-07-08 12:27:37 +0530

    added hero and skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Hero from '@/components/Hero';
+import Skills from '@/components/Skills';
+import { FloatingNav } from '@/components/ui/floating-navbar';
+import { FaHome } from 'react-icons/fa';
+
+const navItems = [
+  { name: 'Home', link: '#home', icon: <FaHome /> },
+  { name: 'Work', link: '#work', icon: <FaHome /> },
+  { name: 'Experience', link: '#experience', icon: <FaHome /> },
+  { name: 'Contact', link: '#contact', icon: <FaHome /> },
+];
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100">
+      <div className="w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <div className="relative -top-10 flex h-20 items-center justify-center">
+          <div className="absolute inset-y-0 w-1 bg-slate-500"></div>
+        </div>
+        <Skills />
+      </div>
+    </main>
+  );
 }
